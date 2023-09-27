35540c8a6277b32055754fab4ad21f1b8de173f6
Pham-Gia-Bao1 <phamgiabao123abc@gmail.com>
2023-09-28 01:41:17 +0700

    bao update product file

--- services/product_detail.js
+++ services/product_detail.js
@@ -1,39 +1,49 @@
-// JavaScript code
-const urlParams = new URLSearchParams(window.location.search);
-const productId = urlParams.get('id');
-// console.log(urlParams)
-// console.log(productId)
-fetch("/database/db.json")
-    .then((res) => res.json())
-    .then((data) => {
-        var product = false;
-        const productList = data.product;
-        data.product.forEach(element => {
-            if (productId == element.id) {
-                product = true;
-                var productDetailHTML = '';
 
-                if (product) {
-                    productDetailHTML = `
-                         <div class="product">
-                             <h2>${element.name}</h2>
-                             <p>Description: ${element.description}</p>
-                             <p>Old Price: ${element.oldPrice}</p>
-                             <p>New Price: ${element.newPrice}</p>
-                             <p>Quantity: ${element.quantity}</p>
-                             <img src="${element.image}" alt="${element.name}">
-                             <img src="${element.image}" alt="${element.name}">
-                             <img src="${element.image}" alt="${element.name}">
-                             <img src="${element.image}" alt="${element.name}">
-
-                         </div>
-                     `;
-
-                    document.getElementById("product-detail").innerHTML = productDetailHTML;
-                } else {
-                    document.getElementById("product-detail").innerHTML = "Product not found.";
-                }
-            }
-
-        });
-    });
+        // JavaScript code
+        const urlParams = new URLSearchParams(window.location.search);
+        const productId = urlParams.get('id');
+        // console.log(urlParams)
+        // console.log(productId)
+        fetch("/database/db.json")
+            .then((res) => res.json())
+            .then((data) => {
+                var product = false;
+                const productList = data.product;
+                data.product.forEach(element => {
+                    if (productId == element.id) {
+                        product = true;
+                        var productDetailHTML = '';
+        
+                        if (product) {
+                            document.getElementById('product__name').innerHTML = element.name;
+                            document.getElementById('main__img').src = element.image;
+                            document.getElementById('secondary__img__item1').src = element.image;
+                            document.getElementById('secondary__img__item2').src = element.image;
+                            document.getElementById('secondary__img__item3').src = element.image;
+                            document.getElementById('description').innerHTML = element.description;
+        
+                            // productDetailHTML = `
+                            
+                            //      <div class="product">
+                            //          <h2>${element.name}</h2>
+                            //          <p>Description: ${element.description}</p>
+                            //          <p>Old Price: ${element.oldPrice}</p>
+                            //          <p>New Price: ${element.newPrice}</p>
+                            //          <p>Quantity: ${element.quantity}</p>
+                            //          <img src="${element.image}" alt="${element.name}">
+                            //          <img src="${element.image}" alt="${element.name}">
+                            //          <img src="${element.image}" alt="${element.name}">
+                            //          <img src="${element.image}" alt="${element.name}">
+        
+                            //      </div>
+                            //  `;
+                             
+        
+                            // document.getElementById("product-detail").innerHTML = productDetailHTML;
+                        } else {
+                            document.getElementById("product-detail").innerHTML = "Product not found.";
+                        }
+                    }
+        
+                });
+            });
