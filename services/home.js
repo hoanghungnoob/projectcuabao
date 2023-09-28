959a131f19ed6acdc7a97b81a1ae0cf7582af2c6
Pham-Gia-Bao1 <phamgiabao123abc@gmail.com>
2023-09-28 10:50:06 +0700

    workinn home.css

--- services/home.js
+++ services/home.js
@@ -1,31 +1,34 @@
+  // JavaScript code
+  fetch("/database/db.json")
+  .then((res) => res.json())
+  .then((data) => {
+      const productList = data.product;
 
-// JavaScript code
-fetch("/database/db.json")
-    .then((res) => res.json())
-    .then((data) => {
-        const productList = data.product;
+      const productHTML = productList.map((product) => {
+          return `
+          <a id="card" href="/page/product_detail/product_detail.html?id=${product.id}">
+              <div class="product">
+                  <img src="${product.image}" alt="${product.name}">
+                  <h2>${product.name}</h2>
+                  <div class="price">
+                      <p>Old Price: ${product.oldPrice}</p>
+                      <p>New Price: ${product.newPrice}</p>
+                  </div>
+                  <div class="descriptiom_and_btn">
+                      <p>${product.description}</p>
+                      <button><img src="/images/img_icon/icon shoping cart.svg" alt="cart"></button>
+                  </div>
+              </div></a>
+             
+          `;
+      });
 
-        const productHTML = productList.map((product) => {
-            return `
-                <div class="product">
-                    <h2>${product.name}</h2>
-                    <p>${product.description}</p>
-                    <p>Old Price: ${product.oldPrice}</p>
-                    <p>New Price: ${product.newPrice}</p>
-                    <p>Quantity: ${product.quantity}</p>
-                    <img src="${product.image}" alt="${product.name}">
-                    <a href="/page/product_detail/product_detail.html?id=${product.id}">Buy</a>
-                  
-                </div>
-            `;
-        });
+      // Gắn nối chuỗi HTML vào phần tử có id "product"
+      document.getElementById("product").innerHTML = `
+          <div class="product-container">
+              ${productHTML.join("")}
+          </div>
+      `;
 
-        // Gắn nối chuỗi HTML vào phần tử có id "product"
-        document.getElementById("product").innerHTML = `
-            <div class="product-container">
-                ${productHTML.join("")}
-            </div>
-        `;
-   
-    });
+  });
 
