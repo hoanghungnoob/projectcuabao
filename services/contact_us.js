8883f59a3b81716cdd336ff33f3518bd08b86c9a
Pham-Gia-Bao1 <phamgiabao123abc@gmail.com>
2023-10-06 20:01:10 +0700

    bao finish contact

--- services/contact_us.js
+++ services/contact_us.js
@@ -1,80 +1,57 @@
-function fetchCustomer() {
-  fetch("http://localhost:3000/customer")
-    .then((res) => res.json())
-    .then((data) => {
-      const productList = data;
-    });
-}
-
 function saveUser(event) {
   event.preventDefault();
+
   const userId = document.querySelector("#userId").value;
   const nameCustomer = document.getElementById("name").value;
   const emailCustomer = document.getElementById("email").value;
   const phoneCustomer = document.getElementById("phone").value;
   const messageCustomer = document.getElementById("message").value;
 
-  if (userId) {
-    fetch(`http://localhost:3000/contact/${userId}`, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        name: nameCustomer,
-        phoneNumber: phoneCustomer,
-        email: emailCustomer,
-        message: messageCustomer,
-      }),
+  const url = userId ? `http://localhost:3000/contact/${userId}` : "http://localhost:3000/contact";
+  const method = userId ? "PUT" : "POST";
+  const data = {
+    name: nameCustomer,
+    phoneNumber: phoneCustomer,
+    email: emailCustomer,
+    message: messageCustomer,
+  };
+
+  fetch(url, {
+    method: method,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => {
+      if (response.ok) {
+        Swal.fire({
+          icon: 'success',
+          title: 'Successfully Sent!',
+          text: 'Thank you for contacting us.',
+          showConfirmButton: true,
+          timer: 3000
+        });
+      } else {
+        Swal.fire({
+          icon: 'error',
+          title: 'Sending Failed!',
+          text: 'Thank you for contacting us.',
+          showConfirmButton: false,
+          timer: 3000
+        });
+      }
     })
-      .then((response) => {
-        if (response.ok) {
-          document.getElementById('successMessage').style.display = "block";
-          
-        } else {
-          document.getElementById('errorMessage').style.display = "block";
-
-        }
-      })
-      .catch((error) => {
-        console.error("Error:", error);
-        document.getElementById('errorMessage').style.display = "block"
-
+    .catch((error) => {
+      Swal.fire({
+        icon: 'error',
+        title: 'Sending Failed!',
+        text: 'Thank you for contacting us.',
+        showConfirmButton: false,
+        timer: 3000
       });
-  } else {
-    fetch("http://localhost:3000/contact", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        name: nameCustomer,
-        phoneNumber: phoneCustomer,
-        email: emailCustomer,
-        message: messageCustomer,
-      }),
-    })
-      .then((response) => {
-        if (response.ok) {
-          document.getElementById('successMessage').style.display = "block";
-          
-
-        } else {
-          document.getElementById('errorMessage').style.display = "block";
-
-        }
-      })
-      .catch((error) => {
-        console.error("Error:", error);
-        document.getElementById('errorMessage').style.display = "block"
-
-      });
-  }
+    });
 }
-
-fetchCustomer();
 
 const contactForm = document.getElementById("contactForm");
 contactForm.addEventListener("submit", saveUser);
-
-
