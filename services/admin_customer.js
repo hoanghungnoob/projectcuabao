0f923cb3129c6352dc8fd2d4fc407494edb49d8f
Pham-Gia-Bao1 <phamgiabao123abc@gmail.com>
2023-10-11 16:30:11 +0700

    bao finish add product

--- services/admin_customer.js
+++ services/admin_customer.js
@@ -13,11 +13,26 @@ function fetchCustomers() {
           <td>${customer.phoneNumber}</td>
           <td>${customer.address}</td>
           <td>
-            <button class="update_btn_product">Update</button>
-            <button class="delete_btn_product">Delete</button>
+            <button id="update_btn_product">Update</button>
+            <button id="delete_btn_product" onclick="delete_customer(${customer.id})">Delete</button>
           </td>
         `;
       });
     });
 }
 fetchCustomers();
+
+
+// delete customer
+function delete_customer(id) {
+  fetch(`http://localhost:3000/customer/${id}`, {
+    method: "DELETE",
+  })
+    .then(() => {
+      alert("Delete success");
+      fetchCustomers(); // Gọi lại hàm fetchCustomers() để cập nhật danh sách khách hàng sau khi xóa
+    })
+    .catch(() => {
+      alert("Delete fail");
+    });
+}
--- services/product_detail.js
+++ services/product_detail.js
@@ -2,21 +2,21 @@ function increment() {
     var inputQty = document.getElementById('input__qty');
     var currentQty = parseInt(inputQty.value) || 0;
     var newQty = currentQty + 1;
+    if (newQty < 1) {
+        newQty = 1;
+    }
     inputQty.value = newQty;
-
 }
 
 function decrement() {
     var inputQty = document.getElementById('input__qty');
     var currentQty = parseInt(inputQty.value) || 0;
     var newQty = currentQty - 1;
-    if (newQty < 0) {
-        newQty = 0;
+    if (newQty < 1) {
+        newQty = 1;
     }
     inputQty.value = newQty;
-
 }
-
 
 // JavaScript code
 const urlParams = new URLSearchParams(window.location.search);
@@ -29,7 +29,6 @@ fetch("http://localhost:3000/product")
         data.forEach(element => {
             if (productId == element.id) {
                 product = true;
-                var productDetailHTML = '';
                 if (product) {
                     document.getElementById('product__name').innerHTML = element.name;
                     document.getElementById('main__img').src = element.image1;
@@ -40,7 +39,10 @@ fetch("http://localhost:3000/product")
                     document.getElementById('old__price').innerHTML = element.oldPrice + " VND";
                     document.getElementById('describe').innerHTML = element.description;
                     // Đánh giá sản phẩm (từ 1 đến 5)
-                    var rating = element.productReviews; // Đây chỉ là ví dụ, bạn có thể thay đổi giá trị này.
+
+                    var rating = element.productReviews; 
+                    console.log(rating)
+                    // Đây chỉ là ví dụ, bạn có thể thay đổi giá trị này.
 
                     // Lấy tất cả các ngôi sao
                     var stars = document.getElementsByClassName("star");
@@ -48,9 +50,8 @@ fetch("http://localhost:3000/product")
                     // Lặp qua từng ngôi sao và làm cho các ngôi sao tương ứng sáng lên
                     for (var i = 0; i < rating; i++) {
                         stars[i].classList.add("selected");
+                        console.log(stars[i])
                     }
-
-
                 } else {
                     document.getElementById("product-detail").innerHTML = "Product not found.";
                 }
@@ -58,10 +59,8 @@ fetch("http://localhost:3000/product")
 
         });
     });
-
-
-    function choise_product(imgs) {
-        main__img.src = imgs.src;
+function choise_product(imgs) {
+    main__img.src = imgs.src;
 }
 
-    
+
