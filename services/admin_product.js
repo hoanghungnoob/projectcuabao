0ecdb94e472a059b42235febf52b60454348db58
Mai <mai.ho25@student.passerellesnumeriques.org>
2023-10-12 14:25:26 +0700

    finish delete addptoduct

--- services/admin_product.js
+++ services/admin_product.js
@@ -37,56 +37,6 @@ function fetchProducts() {
     });
 }
 fetchProducts();
-
-// thêm sản phẩm vào datta
-function addProducts() {
-  var add_productId = document.querySelector("#add_productID").value;
-  var name_addproduct = document.querySelector('#nameInput_add').value;
-  var quantity = document.querySelector('#qtyInput_add').value;
-  var newPrice = document.querySelector('#newPriceInput_add').value;
-  var oldPrice = document.querySelector('#oldPriceInput_add').value;
-  var image_add1 = document.querySelector('#imageFileInput1_add').value;
-  var image_add2 = document.querySelector('#imageFileInput2_add').value;
-  var image_add3 = document.querySelector('#imageFileInput3_add').value;
-  var description = document.querySelector('#descriptionInput_add').value;
-  const content_des = document.querySelector("#myModal_add #content-des").value;
-  var data = {
-    name: name_addproduct,
-    quantity: quantity,
-    newPrice: newPrice,
-    oldPrice: oldPrice,
-    image1: image_add1,
-    image2: image_add2,
-    image3: image_add3,
-    description: description,
-    type: "",
-    productReviews: 4
-  };
-
-  if (add_productId) {
-    fetch(`http://localhost:3000/product/${add_productId}`, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    }).then(() => {
-      fetchUsers();
-    });
-  } else {
-    fetch("http://localhost:3000/product", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    }).then(() => {
-      fetchUsers();
-    });
-  }
-}
-
-
 //  chỉnh sửa sản phẩm 
 function edit_product(id, name, quantity, newPrice, oldPrice, image1, image2, image3, description) {
   // Đặt giá trị vào các trường nhập trong modal chỉnh sửa sản phẩm
