431ca713dc478f3c99a6325182056323812ad3a6
hue <hue.ho25@student.passerellesnumeriques.org>
2023-10-12 15:58:14 +0700

    update customer

--- services/admin_customer.js
+++ services/admin_customer.js
@@ -1,3 +1,43 @@
+// Hàm mở modal và truyền id của khách hàng vào modal
+function openUpdateModal(customerId) {
+  var modal = document.getElementById('exampleModal_Item');
+  var recipientNameInput = document.getElementById('recipient-name_Item');
+  var recipientEmailInput = document.getElementById('recipient-email_Item');
+  var recipientPhoneInput = document.getElementById('recipient-phone_Item');
+  var recipientAddressInput = document.getElementById('recipient-address_Item');
+
+  // Gán giá trị customerId vào thuộc tính data-bs-whatever của nút "Update" trong modal
+  var updateButton = modal.querySelector('.btn-primary');
+  updateButton.setAttribute('data-bs-whatever', customerId);
+
+  // Lấy thông tin khách hàng từ server dựa trên customerId
+  fetch(`http://localhost:3000/customer/${customerId}`)
+    .then(response => response.json())
+    .then(customer => {
+      recipientNameInput.value = customer.name;
+      recipientEmailInput.value = customer.email;
+      recipientPhoneInput.value = customer.phoneNumber;
+      recipientAddressInput.value = customer.address;
+    })
+    .catch(error => {
+      console.log('Error:', error);
+    });
+
+}
+
+
+// Hàm gắn sự kiện click cho nút "Update" của từng khách hàng
+function attachUpdateEventListeners() {
+  var updateButtons = document.querySelectorAll('.update_btn_product');
+  updateButtons.forEach(button => {
+    button.addEventListener('click', function(event) {
+      var customerId = event.target.getAttribute('data-bs-whatever');
+      openUpdateModal(customerId);
+    });
+  });
+}
+
+// Hàm fetch danh sách khách hàng và gắn sự kiện click cho nút "Update"
 function fetchCustomers() {
   fetch('http://localhost:3000/customer')
     .then(response => response.json())
@@ -13,11 +53,18 @@ function fetchCustomers() {
           <td>${customer.phoneNumber}</td>
           <td>${customer.address}</td>
           <td>
-            <button class="update_btn_product">Update</button>
+            <button class="update_btn_product" data-bs-toggle="modal" data-bs-target="#exampleModal_Item" data-bs-whatever="${customer.id}">Update</button>
             <button class="delete_btn_product">Delete</button>
           </td>
         `;
       });
+      // Gắn sự kiện click cho nút "Update" của từng khách hàng
+      attachUpdateEventListeners();
+      
     });
 }
+
+
+// Gọi hàm fetchCustomers để lấy danh sách khách hàng và gắn sự kiện click cho nút "Update" khi tải trang
 fetchCustomers();
+
--- services/admin_product.js
+++ services/admin_product.js
@@ -1,33 +1,50 @@
+function fetchProducts() {
+  fetch('http://localhost:3000/product')
+    .then(response => response.json())
+    .then(data => {
+      var list_product = document.getElementById('list_product');
+      data.forEach(product => {
+        var row = document.createElement("tr");
+        var imageCell = document.getElementById('imageCell');
+        row.innerHTML = `
+          <td>${product.id}</td>
+          <td>${product.name}</td>
+          <td id="imageCell"></td>
+          <td>${product.newPrice}</td>
+          <td>${product.quantity}</td>
+          <td>
+            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#myModal" onclick="saveProduct()">Edit</button>
+            <button class="delete_btn_product btn btn-danger" data-product-id="${product.id}">Delete</button>
+          </td>
+        `;
+        var img = document.createElement('img');
+        img.src = product.image1;
+        img.width = 250;
+        row.querySelector('td:nth-child(3)').appendChild(img);
+        list_product.appendChild(row);
+      });
 
- function fetchProducts(){
-   fetch ('http://localhost:3000/product')
-  .then(respone => respone.json())
-  .then(data =>{
-  var list_product= document.getElementById('list_product');
-  data.forEach(product => {
-    var row= document.createElement("tr");
-    var imageCell = document.getElementById('imageCell');
-    row.innerHTML= `
-      <td>${product.id}</td>
-      <td> ${product.name}</td>
-      <td id="imageCell"></td>
-      <td> ${product.newPrice} </td>
-      <td> ${product.quantity}</td>
-      <td>
-          <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#myModal" onclick="saveProduct()">Edit</button>
-          <button id="delete_btn_product">Delete</button>
-      </td>
-    `;
-    var img = document.createElement('img');
-      img.src = product.image1;
-      img.width = 250; // Thiết lập chiều rộng ảnh
+      var deleteButtons = document.getElementsByClassName('delete_btn_product');
+      for (var i = 0; i < deleteButtons.length; i++) {
+        deleteButtons[i].addEventListener('click', deleteProduct);
+      }
 
-      // Thêm ảnh vào thẻ td trong hàng sản phẩm
-      row.querySelector('td:nth-child(3)').appendChild(img);
-    list_product.appendChild(row);
-  });
-  // Save data to local storage
-    localStorage.setItem('products', JSON.stringify(data));
-});
+      localStorage.setItem('products', JSON.stringify(data));
+    });
 }
+
+function deleteProduct(event) {
+  var productId = event.target.getAttribute('data-product-id');
+
+  fetch(`http://localhost:3000/product/${productId}`, {
+    method: 'DELETE'
+  })
+    .then(() => {
+      location.reload();
+    })
+    .catch(error => {
+      console.error('Error deleting product:', error);
+    });
+}
+
 fetchProducts();
