c3ccbcdad68df94d16428853498af630229eae58
Quy Nguyen <quynguyena52000@gmail.com>
2023-11-04 19:10:21 +0700

    Fix bug

--- services/order.js
+++ services/order.js
@@ -20,40 +20,34 @@ function getUserData() {
   return userData;
 }
 
-const userData = getUserData();
+const userIdentity = getUserData();
 
-
-const user_id = localStorage.getItem('userId');
-const role_Id = localStorage.getItem('roleId')
-console.log(user_id)
-
-
-// Fetch dữ liệu tỉnh từ localhost
-fetchData(provinceUrl)
-  .then(data => {
+fetch(provinceUrl)
+  .then((data) => data.json())
+  .then((data) => {
+    console.log(data);
     const provinces = data;
-    // Đẩy dữ liệu tỉnh vào select tỉnh
     provinces.forEach(function (province) {
-      var option = document.createElement('option');
+      var option = document.createElement("option");
       option.value = province.code;
       option.text = province.name;
       provinceSelect.appendChild(option);
     });
 
-    // Xử lý sự kiện khi chọn tỉnh
-    provinceSelect.addEventListener('change', function () {
+    provinceSelect.addEventListener("change", function () {
       var selectedProvinceCode = provinceSelect.value;
 
       // Fetch dữ liệu huyện từ localhost dựa trên mã tỉnh đã chọn
-      fetchData(`${districtUrl}?parent_code=${selectedProvinceCode}`)
-        .then(data => {
+      fetch(`${districtUrl}?parent_code=${selectedProvinceCode}`)
+        .then((data) => data.json())
+        .then((data) => {
           const districts = data;
           // Xóa tất cả các option cũ trong select huyện
-          districtSelect.innerHTML = '';
+          districtSelect.innerHTML = "";
 
           // Đẩy dữ liệu huyện vào select huyện
           districts.forEach(function (district) {
-            var option = document.createElement('option');
+            var option = document.createElement("option");
             option.value = district.code;
             option.text = district.name;
             districtSelect.appendChild(option);
@@ -64,22 +58,17 @@ fetchData(provinceUrl)
 //chuyển về trang order khi nhất button
 function redirectToOrderPage(productId) {
   window.location.href = `/page/order/order.html?id=${productId}`;
-
 }
 
-
-
 const urlParams3 = new URLSearchParams(window.location.search);
-const productId1 = urlParams3.get('id');
+const productId1 = urlParams3.get("id");
 const list_id = [];
-list_id.push(productId1)
-console.log(list_id,"hello")
+list_id.push(productId1);
 
 function product_order_detail() {
-
   // JavaScript code
   const urlParams = new URLSearchParams(window.location.search);
-  const productId = urlParams.get('id');
+  const productId = urlParams.get("id");
 
   fetch("http://localhost:3000/products")
     .then((res) => res.json())
@@ -87,102 +76,104 @@ function product_order_detail() {
       var product = false;
       const productList = data;
 
-      productList.forEach(element => {
+      productList.forEach((element) => {
         if (productId == element.id) {
           product = true;
-          var productDetailHTML = '';
+          var productDetailHTML = "";
 
           if (product) {
-            document.getElementById('price__order').innerHTML = element.newPrice * quantity_order_after + " VND";
-            document.getElementById('product__img__order').src = element.image1;
-            document.getElementById('procuct__name__order').innerHTML = element.name;
-            document.getElementById('product__price__order').innerHTML = element.newPrice + " VND";
-            document.getElementById('product__quantity__order').innerHTML = document.get('input__qty').value;
+            document.getElementById("price__order").innerHTML =
+              element.newPrice * quantity_order_after + " VND";
+            document.getElementById("product__img__order").src = element.image1;
+            document.getElementById("procuct__name__order").innerHTML =
+              element.name;
+            document.getElementById("product__price__order").innerHTML =
+              element.newPrice + " VND";
+            document.getElementById("product__quantity__order").innerHTML =
+              document.get("input__qty").value;
           } else {
-            document.getElementById("product-detail").innerHTML = "Product not found.";
+            document.getElementById("product-detail").innerHTML =
+              "Product not found.";
           }
         }
-
       });
     });
-
 }
 var urlParams1 = new URLSearchParams(window.location.search);
-var quantity_detail = urlParams1.get('quantity');
+var quantity_detail = urlParams1.get("quantity");
 
 if (quantity_detail == null || quantity_detail == NaN) {
-  var quantity_order_after = document.getElementById("quantity_order").innerHTML = 1;
-}
-else {
-  var quantity_order_after = document.getElementById("quantity_order").innerHTML = quantity_detail;
+  var quantity_order_after = (document.getElementById(
+    "quantity_order"
+  ).innerHTML = 1);
+} else {
+  var quantity_order_after = (document.getElementById(
+    "quantity_order"
+  ).innerHTML = quantity_detail);
 }
 
 function useCustomerData(customerId) {
-   if(role_Id == 2){
+  if (userIdentity?.roleId == 2) {
     fetch(`http://localhost:3000/users?id=${customerId}`)
-    .then((res) => res.json())
-    .then((data) => {
-      data.forEach((element) => {
-        document.getElementById('name__order').value = element.name;
-        document.getElementById('customerID__order').value = element.id;
-        document.getElementById('email__order').value = element.email;
-        document.getElementById('phone__order').value = element.phoneNumber;
-        document.getElementById('address__order').value = element.address;
-        // Gán giá trị mặc định cho các ô select province và district
-        document.getElementById('date__order').value = getCurrentTime();
-
+      .then((res) => res.json())
+      .then((data) => {
+        data.forEach((element) => {
+          document.getElementById("name__order").value = element.name;
+          document.getElementById("customerID__order").value = element.id;
+          document.getElementById("email__order").value = element.email;
+          document.getElementById("phone__order").value = element.phoneNumber;
+          document.getElementById("address__order").value = element.address;
+          // Gán giá trị mặc định cho các ô select province và district
+          document.getElementById("date__order").value = getCurrentTime();
+        });
       })
-    })
-    .catch((error) => {
-    });
-   }
-   else{
+      .catch((error) => {});
+  } else {
     fetch(`http://localhost:3000/users?id=${customerId}`)
-    .then((res) => res.json())
-    .then((data) => {
-      data.forEach((element) => {
-        document.getElementById('name__order').value = element.name;
-        document.getElementById('customerID__order').value = element.id;
-        document.getElementById('email__order').value = element.email;
-        document.getElementById('phone__order').value = element.phoneNumber;
-        document.getElementById('address__order').value = element.address;
-        // Gán giá trị mặc định cho các ô select province và district
-        document.getElementById('date__order').value = getCurrentTime();
-
+      .then((res) => res.json())
+      .then((data) => {
+        data.forEach((element) => {
+          document.getElementById("name__order").value = element.name;
+          document.getElementById("customerID__order").value = element.id;
+          document.getElementById("email__order").value = element.email;
+          document.getElementById("phone__order").value = element.phoneNumber;
+          document.getElementById("address__order").value = element.address;
+          // Gán giá trị mặc định cho các ô select province và district
+          document.getElementById("date__order").value = getCurrentTime();
+        });
       })
-    })
-    .catch((error) => {
-    });
-   }
+      .catch((error) => {});
+  }
 }
 
-useCustomerData(user_id);
+useCustomerData(userIdentity.id);
 
 function getDataFormOrder() {
   product_order_detail();
-  var fullName = document.getElementById('name__order').value;
-  var customerID = document.getElementById('customerID__order').value;
+  var fullName = document.getElementById("name__order").value;
+  var customerID = document.getElementById("customerID__order").value;
 
-  var productId2 ;
+  var productId2;
   var productId_list_id = [];
-  var cartItems = JSON.parse(localStorage.getItem('selectedIds'));
+  var cartItems = JSON.parse(localStorage.getItem("selectedIds"));
   if (cartItems && Array.isArray(cartItems)) {
     productId_list_id = cartItems;
     productId2 = productId_list_id;
   } else {
-     productId2 = list_id;
+    productId2 = list_id;
   }
-  var email = document.getElementById('email__order').value;
-  var phoneNumber = document.getElementById('phone__order').value;
-  var address = document.getElementById('address__order').value;
-  var province = document.getElementById('province__order').value;
-  var district = document.getElementById('district__order').value;
-  var productName = document.getElementById('procuct__name__order').textContent;
-  var productPrice = document.getElementById('product__price__order').textContent;
-  var quantity = document.getElementById('quantity_order').textContent;
-  var totalPrice = document.getElementById('price__order').textContent;
-  var date = document.getElementById('date__order').value;
-
+  var email = document.getElementById("email__order").value;
+  var phoneNumber = document.getElementById("phone__order").value;
+  var address = document.getElementById("address__order").value;
+  var province = document.getElementById("province__order").value;
+  var district = document.getElementById("district__order").value;
+  var productName = document.getElementById("procuct__name__order").textContent;
+  var productPrice = document.getElementById(
+    "product__price__order"
+  ).textContent;
+  var quantity = document.getElementById("quantity_order").textContent;
+  var totalPrice = document.getElementById("price__order").textContent;
+  var date = document.getElementById("date__order").value;
 
   var data = {
     name: fullName,
@@ -197,22 +188,26 @@ function getDataFormOrder() {
     productPrice: productPrice,
     quantity: quantity,
     totalPrice: totalPrice,
-    date: date
+    date: date,
   };
 
   return data;
 }
-getDataFormOrder()
+getDataFormOrder();
 
 function placeOrder() {
-  if (user_id) {
-    // Đã có userId trong LocalStorage
-
-    // Tiếp tục thực hiện đơn hàng
+  if (userIdentity.id) {
     var data = getDataFormOrder();
 
-    // Kiểm tra các trường nhập liệu bắt buộc
-    if (!data.name || !data.email || !data.phoneNumber || !data.address || !data.province || !data.district || !data.quantity) {
+    if (
+      !data.name ||
+      !data.email ||
+      !data.phoneNumber ||
+      !data.address ||
+      !data.province ||
+      !data.district ||
+      !data.quantity
+    ) {
       alert("Vui lòng nhập đầy đủ thông tin.");
       return;
     }
@@ -248,35 +243,37 @@ function placeOrder() {
 
       // Định dạng lại đối tượng thời gian
       if (month < 10) {
-        month = '0' + month;
+        month = "0" + month;
       }
       if (day < 10) {
-        day = '0' + day;
+        day = "0" + day;
       }
 
-      var formattedDate = year + '-' + month + '-' + day;
+      var formattedDate = year + "-" + month + "-" + day;
       date__order = formattedDate;
       document.getElementById("date__order").innerHTML = date__order;
-
     }
 
     getCurrentTime();
   } else {
     // Không có userId trong LocalStorage
-    alert('Bạn phải đăng nhập!');
+    alert("Bạn phải đăng nhập!");
   }
 }
 
-// Xử lý sự kiện thay đổi Local Storage
-// Lấy dữ liệu từ Local Storage
-var selectedQuantities = JSON.parse(localStorage.getItem('selectedQuantities'));
-var cartItems = JSON.parse(localStorage.getItem('selectedIds'));
+var selectedQuantities = JSON.parse(localStorage.getItem("selectedQuantities"));
+var cartItems = JSON.parse(localStorage.getItem("selectedIds"));
 function handleLocalStorageChange() {
-  const updatedQuantities = JSON.parse(localStorage.getItem('selectedQuantities'));
-  const updatedCartItems = JSON.parse(localStorage.getItem('selectedIds'));
+  const updatedQuantities = JSON.parse(
+    localStorage.getItem("selectedQuantities")
+  );
+  const updatedCartItems = JSON.parse(localStorage.getItem("selectedIds"));
 
   // Kiểm tra xem có sự thay đổi trong Local Storage không
-  if (JSON.stringify(selectedQuantities) !== JSON.stringify(updatedQuantities) || JSON.stringify(cartItems) !== JSON.stringify(updatedCartItems)) {
+  if (
+    JSON.stringify(selectedQuantities) !== JSON.stringify(updatedQuantities) ||
+    JSON.stringify(cartItems) !== JSON.stringify(updatedCartItems)
+  ) {
     selectedQuantities = updatedQuantities;
     cartItems = updatedCartItems;
 
@@ -286,20 +283,20 @@ function handleLocalStorageChange() {
 }
 
 // Xử lý sự kiện storage khi có thay đổi trong Local Storage
-window.addEventListener('storage', handleLocalStorageChange);
+window.addEventListener("storage", handleLocalStorageChange);
 
 // Xử lý sự kiện beforeunload khi rời khỏi trang
-window.addEventListener('beforeunload', () => {
+window.addEventListener("beforeunload", () => {
   // Xóa dữ liệu trong Local Storage
-  localStorage.removeItem('selectedQuantities');
-  localStorage.removeItem('selectedIds');
+  localStorage.removeItem("selectedQuantities");
+  localStorage.removeItem("selectedIds");
 });
 
 // Cập nhật lại giao diện đơn hàng
 function updateOrderPage() {
-  const productOrderElement = document.getElementById('product__order1');
-  const totalPriceElement = document.getElementById('price__order');
-  let productsHTML = '';
+  const productOrderElement = document.getElementById("product__order1");
+  const totalPriceElement = document.getElementById("price__order");
+  let productsHTML = "";
   let totalPrice = 0;
 
   fetch("http://localhost:3000/products")
@@ -315,7 +312,9 @@ function updateOrderPage() {
             productsHTML += `
               <div class="product__infor__order">
                 <div class="image__order">
-                  <img id="product__img__order" src="${element.image1}" alt="Product">
+                  <img id="product__img__order" src="${
+                    element.image1
+                  }" alt="Product">
                 </div>
                 <p id="procuct__name__order">${element.name}</p>
                 <p id="product__price__order">${element.newPrice}</p>
